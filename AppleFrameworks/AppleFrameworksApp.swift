bc74d214a59a7df5290b641b785b6248e33d33d8
architanemalikanti <apn32@cornell.edu>
2025-06-04 16:31:08 -0700

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Archita Nemalikanti on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
